626486ff136130a4ca83e48e9ff00b50f9645cfa
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-19 15:36:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const name = "Joe";
-const height = 74;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
+
+// module.exports = {name, height, message};
+
+// console.log (message)
+
+// describe("Name", () => {
+//     it('returns "Susan"', () => {
+//       expect(name).toEqual("Susan");
+//     });
+//   });
+
+const name ="susan";
+const height = "less than 40";
+
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+console.log (message)
